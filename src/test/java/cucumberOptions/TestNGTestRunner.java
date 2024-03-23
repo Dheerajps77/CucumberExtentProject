bd7b606a101dfc74acf34bf49f01a7715a1ebbc9
therahul413 <therahul413@gmail.com>
2024-03-23 20:53:57 +0530

    ForgetPassword

--- src/test/java/cucumberOptions/TestNGTestRunner.java
+++ src/test/java/cucumberOptions/TestNGTestRunner.java
@@ -14,7 +14,11 @@ import io.cucumber.testng.CucumberOptions;
 				"html:target/cucumber.html", 
 				"json:target/cucumber.json",
 				"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
-				"rerun:target/failed_scenarios.txt" }
+				"rerun:target/failed_scenarios.txt",
+				"html:target/cucumber-html-report", 
+				"json:target/cucumber.json",
+				"pretty:target/cucumber-pretty.txt",
+				"usage:target/cucumber-usage.json"}
 		)
 public class TestNGTestRunner extends AbstractTestNGCucumberTests {
 
